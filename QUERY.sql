f4f9f739a77f6b6ae0a9b9ac8f8876059705c8a9
HoridasPro <horidassarker6@gmail.com>
2026-06-14 13:36:36 +0600

    added to users data insert

--- QUERY.sql
+++ QUERY.sql
@@ -9,3 +9,12 @@ create table Users(
   role varchar(20) check (role in ('Ticket Manager', 'Football Fan')) not null,
   phone_number varchar(20) 
 )
+
+--users data insert
+ insert  into Users (user_id, full_name, email, role, phone_number)  values
+(1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
+(2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
+(3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
+(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
+
+
