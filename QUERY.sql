1cde3edd05fedddb3fc29899b234b8fcceee9783
HoridasPro <horidassarker6@gmail.com>
2026-06-14 16:53:19 +0600

    added to matches table and insert data

--- QUERY.sql
+++ QUERY.sql
@@ -17,16 +17,78 @@ create table Users(
 (3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
 (4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
 
+--create matches table
+create table Matches (
+  match_id int primary key,
+  fixture varchar(100) not null,
+  tournament_category varchar(100) not null,
+  base_ticket_price int check (base_ticket_price > 0) not null,
+  match_status varchar(20) check (
+    match_status in (
+      'Available',
+      'Selling Fast',
+      'Sold Out',
+      'Postponed'
+    )
+  ) not null
+)
+
+
+--matches data insert
+insert into
+  Matches (
+    match_id,
+    fixture,
+    tournament_category,
+    base_ticket_price,
+    match_status
+  )
+values
+  (
+    101,
+    'Real Madrid vs Barcelona',
+    'Champions League',
+    150.00,
+    'Available'
+  ),
+  (
+    102,
+    'Man City vs Liverpool',
+    'Premier League',
+    120.00,
+    'Selling Fast'
+  ),
+  (
+    103,
+    'Bayern Munich vs PSG',
+    'Champions League',
+    130.00,
+    'Available'
+  ),
+  (
+    104,
+    'AC Milan vs Inter Milan',
+    'Serie A',
+    90.00,
+    'Sold Out'
+  ),
+  (
+    105,
+    'Juventus vs Roma',
+    'Serie A',
+    80.00,
+    'Available'
+  );
 --create booking table
 create table Bookings (
-  booking_id INT primary key,
-  user_id INT references Users (user_id) ON delete cascade,
-  match_id INT references Matches (match_id) ON delete cascade,
+  booking_id int primary key,
+  user_id int references Users (user_id) ON delete cascade,
+  match_id int references Matches (match_id) ON delete cascade,
   seat_number VARCHAR(10),
   payment_status VARCHAR(20) check (
     payment_status IN ('Pending', 'Confirmed', 'Cancelled', 'Refunded')
   ),
-  total_cost INT not null check (total_cost > 0)
+  total_cost int not null check (total_cost > 0)
 );
 
 --bookings data insert
