1a4a3dcfdb6702aeeb7b266af329695afc2df6c1
HoridasPro <horidassarker6@gmail.com>
2026-06-14 18:43:56 +0600

    added to query -3

--- QUERY.sql
+++ QUERY.sql
@@ -114,3 +114,14 @@ where
   --Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
 select user_id, full_name, email from Users
 where full_name ilike 'Tanvir%' or full_name ilike '%Haque%';
+
+--Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+select
+  booking_id,
+  user_id,
+  match_id,
+  coalesce(payment_status, 'Action Required') as systematic_status
+from
+  Bookings
+where
+  payment_status is null
